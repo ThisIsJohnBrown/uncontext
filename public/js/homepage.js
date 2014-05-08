da374a5fdf9e947788f0f09f7625af9c938e1dd3
John Brown <thisisjohnbrown@gmail.com>
2014-05-07 21:27:12 -0700

    updates to homepage scene and newrelic

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -11,8 +11,9 @@ $(function() {
 
   uncontext.socket_.on('0', function (data) {
     if (socketData.a !== data.a) {
+      var now = new Date().getTime();
       for (var i = 0; i < data.a; i++) {
-        var item = addItem(i, data.a, data.b, data.c, data.e.f / data.e.g);
+        var item = addItem(i, data.a, data.b, data.c, data.e.f / data.e.g, now);
         items_.push(item);
       }
     }
@@ -21,35 +22,30 @@ $(function() {
   });
 })
 
-function addItem(i, iTotal, size, direction, yOffset) {
+function addItem(i, iTotal, size, direction, yOffset, now) {
   var item = {};
   item.position = [direction ? i/iTotal : 1 - i/iTotal, yOffset];
   item.finalSize = size;
   item.currSize = 0;
-  item.start = new Date().getTime() + i * 100;
-  item.delay = i;
+  item.start = now;
+  item.delay = i * 100;
   return item;
 }
 
 function init() {
   canvas = document.getElementById('homepage-canvas');
-  // canvas.width = window.innerWidth;
-  // canvas.height = window.innerHeight;
   context = canvas.getContext('2d');
-  // window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function animate() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   var now = new Date().getTime();
 
-  if (items_.length > 200) {
-    items_ = [];
-  }
-
   for (var i = items_.length - 1; i >= 0; i--) {
     var item = items_[i];
-    if (item.start < now) {
+    if (item.start + item.delay + 1000 < now) {
+      items_.splice(i, 1);
+    } else if (item.start + item.delay < now) {
       if (item.currSize < item.finalSize) {
         item.currSize += speed;
         context.beginPath();
